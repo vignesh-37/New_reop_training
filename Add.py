29211ccddd95d28b3712189dc7be597a9831f582
vignesh-37 <vigneshvengadesh034@gmail.com>
2024-07-01 20:06:51 +0530

    I descibe the work what i done in the repo in the exaple txt file

--- Add.py
+++ Add.py
@@ -1,5 +1,5 @@
-//Addition operation python
-number1=int(input("Enter the First Number:")
-number2=int(input("Enter the Second Number:")
+#Addition operation python
+number1=int(input("Enter the First Number:"))
+number2=int(input("Enter the Second Number:"))
 res=number1+number2
 print("The Result Is:",res)
